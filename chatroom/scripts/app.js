fa99aef3c4158921778266a89915fc62c556f622
Logan Kovash <l.kovash@yahoo.com>
2021-05-06 22:24:04 -0600

    updates to chatroom, addition of username change

--- chatroom/scripts/app.js
+++ chatroom/scripts/app.js
@@ -5,10 +5,35 @@
  */
 //dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
+
+//update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 2000);
+});
+
+// Check localstorage for a username
+const username = localStorage.username ? localStorage.username : 'Anonymous';
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'Logan');
+const chatroom = new Chatroom('general', username);
 
 //get chats and render
 chatroom.getChats(data => chatUI.render(data));
